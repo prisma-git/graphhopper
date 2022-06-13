caa3ae34d4c18e6ba178e66a8728ed1305b7195e
florian.kuster <florian.kuster@prisma-solutions.at>
2022-06-13 11:57:54 +0200

    shebang

--- docker/graphhopper.sh
+++ docker/graphhopper.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 java -Ddw.graphhopper.datareader.file=/opt/graphhopper/data/source/dach-latest.osm2.pbf -jar graphhopper-web-5.0-SNAPSHOT.jar server /opt/graphhopper/data/config/config.yml
