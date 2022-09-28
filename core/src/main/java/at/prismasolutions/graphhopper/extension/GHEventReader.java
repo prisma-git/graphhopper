248b0a234b791e527003426840f40dee43ab3ab8
florian.kuster <florian.kuster@prisma-solutions.at>
2022-09-28 14:13:14 +0200

    fix reading issue

--- core/src/main/java/at/prismasolutions/graphhopper/extension/GHEventReader.java
+++ core/src/main/java/at/prismasolutions/graphhopper/extension/GHEventReader.java
@@ -24,106 +24,99 @@ public class GHEventReader {
 		List<GHEvent> list = new ArrayList<GHEvent>();
 		try (JsonParser jParser = new JsonFactory().createParser(file);) {
 			// loop until token equal to "}"
-			while (jParser.nextToken() != JsonToken.END_OBJECT) {
-				String fieldname = jParser.getCurrentName();
-				if ("ghEvents".equals(fieldname)) {
-					if (jParser.nextToken() == JsonToken.START_ARRAY) {
-						// ghEvents is array, loop until token equal to "]"
-						while (jParser.nextToken() != JsonToken.END_ARRAY) {
-							if (jParser.getCurrentToken() == JsonToken.START_OBJECT) {
-								GHEvent event = new GHEvent();
-								while (jParser.nextToken() != JsonToken.END_OBJECT) {
-									String eventfieldname = jParser.getCurrentName();
-									if ("type".equals(eventfieldname)) {
-										int type = jParser.nextIntValue(0);
-										switch (type) {
-										case 0:
-											event.setType(GHEventType.ALL);
-											break;
-										case 1:
-											event.setType(GHEventType.DESCRIPTION);
-											break;
-										case 2:
-											event.setType(GHEventType.EQUAL);
-											break;
-										case 3:
-											event.setType(GHEventType.LESSERTHAN);
-											break;
-										case 4:
-											event.setType(GHEventType.GREATERTHAN);
-											break;
-										}
-									}
-									if ("startDate".equals(eventfieldname)) {
-										if (jParser.getCurrentValue() != null) {
-											String date = jParser.nextTextValue();
-											OffsetDateTime temp = OffsetDateTime.parse(date,
-													DateTimeFormatter.ISO_DATE_TIME);
-											event.setStartDate(Instant.from(temp));
-										}
-									}
-									if ("endDate".equals(eventfieldname)) {
-										if (jParser.getCurrentValue() != null) {
-											String date = jParser.nextTextValue();
-											OffsetDateTime temp = OffsetDateTime.parse(date,
-													DateTimeFormatter.ISO_DATE_TIME);
-											event.setEndDate(Instant.from(temp));
-										}
-									}
-									if ("direction".equals(eventfieldname)) {
-										int val = jParser.nextIntValue(2);
-										event.setDirection((short) val);
-									}
-									if ("statFrom".equals(eventfieldname)) {
-										jParser.nextToken();
-										double val = jParser.getDoubleValue();
-										event.setStatFrom(val);
-									}
-									if ("statTo".equals(eventfieldname)) {
-										jParser.nextToken();
-										double val = jParser.getDoubleValue();
-										event.setStatTo(val);
-									}
-									if ("caption".equals(eventfieldname)) {
-										String val = jParser.nextTextValue();
-										event.setCaption(val);
-									}
-									if ("extType".equals(eventfieldname)) {
-										String val = jParser.nextTextValue();
-										event.setExtType(val);
-									}
-									if ("extId".equals(eventfieldname)) {
-										String val = jParser.nextTextValue();
-										event.setExtId(val);
-									}
-									if ("extEdgeId".equals(eventfieldname)) {
-										String val = jParser.nextTextValue();
-										event.setExtEdgeId(val);
-									}
-									if ("shape".equals(eventfieldname)) {
-										String val = jParser.nextTextValue();
-										event.setShape(val);
-									}
-									if ("parameterName".equals(eventfieldname)) {
-										String val = jParser.nextTextValue();
-										event.setParameterName(val);
-									}
-									if ("parameterValue".equals(eventfieldname)) {
-										jParser.nextToken();
-										double val = jParser.getValueAsDouble();
-										event.setParameterValue(val);
-									}
-									if ("factor".equals(eventfieldname)) {
-										jParser.nextToken();
-										double val = jParser.getDoubleValue();
-										event.setFactor(val);
-									}
-								}
-								list.add(event);
+
+			// ghEvents is array, loop until token equal to "]"
+			while (jParser.nextToken() != JsonToken.END_ARRAY) {
+				if (jParser.getCurrentToken() == JsonToken.START_OBJECT) {
+					GHEvent event = new GHEvent();
+					while (jParser.nextToken() != JsonToken.END_OBJECT) {
+						String eventfieldname = jParser.getCurrentName();
+						if ("type".equals(eventfieldname)) {
+							int type = jParser.nextIntValue(0);
+							switch (type) {
+							case 0:
+								event.setType(GHEventType.ALL);
+								break;
+							case 1:
+								event.setType(GHEventType.DESCRIPTION);
+								break;
+							case 2:
+								event.setType(GHEventType.EQUAL);
+								break;
+							case 3:
+								event.setType(GHEventType.LESSERTHAN);
+								break;
+							case 4:
+								event.setType(GHEventType.GREATERTHAN);
+								break;
 							}
 						}
+						if ("startDate".equals(eventfieldname)) {
+							if (jParser.getCurrentValue() != null) {
+								String date = jParser.nextTextValue();
+								OffsetDateTime temp = OffsetDateTime.parse(date, DateTimeFormatter.ISO_DATE_TIME);
+								event.setStartDate(Instant.from(temp));
+							}
+						}
+						if ("endDate".equals(eventfieldname)) {
+							if (jParser.getCurrentValue() != null) {
+								String date = jParser.nextTextValue();
+								OffsetDateTime temp = OffsetDateTime.parse(date, DateTimeFormatter.ISO_DATE_TIME);
+								event.setEndDate(Instant.from(temp));
+							}
+						}
+						if ("direction".equals(eventfieldname)) {
+							int val = jParser.nextIntValue(2);
+							event.setDirection((short) val);
+						}
+						if ("statFrom".equals(eventfieldname)) {
+							jParser.nextToken();
+							double val = jParser.getDoubleValue();
+							event.setStatFrom(val);
+						}
+						if ("statTo".equals(eventfieldname)) {
+							jParser.nextToken();
+							double val = jParser.getDoubleValue();
+							event.setStatTo(val);
+						}
+						if ("caption".equals(eventfieldname)) {
+							String val = jParser.nextTextValue();
+							event.setCaption(val);
+						}
+						if ("extType".equals(eventfieldname)) {
+							String val = jParser.nextTextValue();
+							event.setExtType(val);
+						}
+						if ("extId".equals(eventfieldname)) {
+							String val = jParser.nextTextValue();
+							event.setExtId(val);
+						}
+						if ("extEdgeId".equals(eventfieldname)) {
+							String val = jParser.nextTextValue();
+							event.setExtEdgeId(val);
+						}
+						if ("shape".equals(eventfieldname)) {
+							String val = jParser.nextTextValue();
+							event.setShape(val);
+						}
+						if ("parameterName".equals(eventfieldname)) {
+							String val = jParser.nextTextValue();
+							event.setParameterName(val);
+						}
+						if ("parameterValue".equals(eventfieldname)) {
+							jParser.nextToken();
+							double val = jParser.getValueAsDouble();
+							event.setParameterValue(val);
+						}
+						if ("factor".equals(eventfieldname)) {
+							jParser.nextToken();
+							double val = jParser.getDoubleValue();
+							event.setFactor(val);
+						}
 					}
+					list.add(event);
 				}
+
 			}
 		} catch (JsonGenerationException e) {
 			logger.error("Error reading GHEvents", e);
