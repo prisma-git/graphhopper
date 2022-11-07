64a85fd47797a9b1d9f905bf0e1a97910196c64e
florian.kuster <florian.kuster@prisma-solutions.at>
2022-11-07 11:10:56 +0100

    remove exlplicit import

--- docker/graphhopper.sh
+++ docker/graphhopper.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-java -Xmx10g -Xms10g -jar /opt/graphhopper/graphhopper-web-5.0-SNAPSHOT.jar import /opt/graphhopper/data/config/config.yml
 java -Xmx10g -Xms10g -jar /opt/graphhopper/graphhopper-web-5.0-SNAPSHOT.jar server /opt/graphhopper/data/config/config.yml
