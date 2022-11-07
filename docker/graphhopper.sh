3b550f65b0af0e22d94b23730a348a2a031c97cf
florian.kuster <florian.kuster@prisma-solutions.at>
2022-11-07 13:30:02 +0100

    lower mem

--- docker/graphhopper.sh
+++ docker/graphhopper.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xmx10g -Xms10g -jar /opt/graphhopper/graphhopper-web-5.0-SNAPSHOT.jar server /opt/graphhopper/data/config/config.yml
+java -Xmx7g -Xms7g -jar /opt/graphhopper/graphhopper-web-5.0-SNAPSHOT.jar server /opt/graphhopper/data/config/config.yml
