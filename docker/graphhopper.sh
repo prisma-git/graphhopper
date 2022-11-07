e47e6de482ab092c50048d0d4bdbef34f69231d0
florian.kuster <florian.kuster@prisma-solutions.at>
2022-11-07 10:46:54 +0100

    more memory

--- docker/graphhopper.sh
+++ docker/graphhopper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-java -jar /opt/graphhopper/graphhopper-web-5.0-SNAPSHOT.jar server /opt/graphhopper/data/config/config.yml
+java -Xmx10g -Xms10g -jar /opt/graphhopper/graphhopper-web-5.0-SNAPSHOT.jar import /opt/graphhopper/data/config/config.yml
+java -Xmx10g -Xms10g -jar /opt/graphhopper/graphhopper-web-5.0-SNAPSHOT.jar server /opt/graphhopper/data/config/config.yml
