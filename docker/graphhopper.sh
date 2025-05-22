0090021da1519400b23535e048f0a06d85c85dc0
prisma-git <programmierer@prisma-solutions.at>
2025-05-22 13:40:05 +0200

    Update graphhopper.sh

--- docker/graphhopper.sh
+++ docker/graphhopper.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+mkdir -p /opt/graphhopper/data/graph-cache
+chgrp -R work /opt/graphhopper/data/graph-cache
+chmod -R 664 /opt/graphhopper/data/graph-cache
 java -Xmx7g -Xms7g -jar /opt/graphhopper/graphhopper-web-5.0-SNAPSHOT.jar server /opt/graphhopper/data/config/config.yml
