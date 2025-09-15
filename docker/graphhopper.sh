3b5b94b615ae8f6578a82a5b1bb69d9ddbbd2a8e
prisma-git <programmierer@prisma-solutions.at>
2025-09-15 17:39:50 +0200

    increase memory

--- docker/graphhopper.sh
+++ docker/graphhopper.sh
@@ -2,4 +2,4 @@
 mkdir -p /opt/graphhopper/data/graph-cache
 chgrp -R work /opt/graphhopper/data/graph-cache
 chmod -R 664 /opt/graphhopper/data/graph-cache
-java -Xmx7g -Xms7g -jar /opt/graphhopper/graphhopper-web-5.0-SNAPSHOT.jar server /opt/graphhopper/data/config/config.yml
+java -Xmx10g -Xms10g -jar /opt/graphhopper/graphhopper-web-11.0-SNAPSHOT.jar server /opt/graphhopper/data/config/config.yml
